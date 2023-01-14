1ca4acf3e0db4af7b45242c206c123e73e0626f5
francois delattre <francoisdel@gmail.com>
2023-01-14 16:09:50 +0100

    ajout d une colonne au modele boat

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_14_133643) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_14_142759) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_14_133643) do
     t.text "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_14_133643) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
 
+  add_foreign_key "boats", "users"
 end
