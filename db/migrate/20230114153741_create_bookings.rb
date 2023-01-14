4675085c13e0be175bfebc34e8066cb0e8a6f2f9
francois delattre <francoisdel@gmail.com>
2023-01-14 16:38:58 +0100

    un petit commit

--- db/migrate/20230114153741_create_bookings.rb
+++ db/migrate/20230114153741_create_bookings.rb
@@ -1,8 +1,8 @@
 class CreateBookings < ActiveRecord::Migration[7.0]
   def change
     create_table :bookings do |t|
-      t.references :boat_id, null: false, foreign_key: true
-      t.references :user_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :boat, null: false, foreign_key: true
       t.datetime :start_date
       t.datetime :end_date
 
