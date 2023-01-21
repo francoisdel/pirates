dd8701f831b14525edb97b2ce0234c5d42bbced1
francois delattre <francoisdel@gmail.com>
2023-01-21 11:44:49 +0100

    le form du booking
    Signed-off-by: francois delattre <francoisdel@gmail.com>

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,33 @@
 class BookingsController < ApplicationController
+  before_action :set_boat, only: %i[new create]
   def new
     @boat = Boat.find(params[:boat_id])
     @booking = Booking.new
   end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.boat = @boat
+    @booking.user = current_user
+    @booking.save
+    redirect_to boat_path(@boat)
+  end
+
+
+  def show
+    @booking =  Boat.find(params[:id])
+
+  end
+
+  private
+
+  def set_boat
+    @boat = Boat.find(params[:boat_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+
 
 end
